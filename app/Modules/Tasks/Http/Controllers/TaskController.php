0c1060e1ea31e5d3e0fe9aea17c5dd149b2ce8a5
aliiahmadi <aliahmadi.github@gmail.com>
2023-04-04 16:26:10 +0330

    API methods

--- app/Modules/Tasks/Http/Controllers/TaskController.php
+++ app/Modules/Tasks/Http/Controllers/TaskController.php
@@ -32,7 +32,7 @@ class TaskController extends Controller
 
         $task = $taskService->createTask($taskDTO);
 
-        // return response()->
+        return response()->json($task, 201);
     }
 
     /**
@@ -54,16 +54,40 @@ class TaskController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, string $id, TaskServiceInterface $taskService)
     {
-        //
+        $taskDTO = new TaskDTO(
+            $request->input("title"),
+            $request->input("description")
+        );
+
+        $task = $taskService->updateTask($id, $taskDTO);
+
+        if (!$task) {
+            return response()->json([
+                "message" => "Task Not Found."
+            ], 404);
+        }
+
+        return response()->json($task);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(string $id, TaskServiceInterface $taskService)
     {
-        //
+        $task = $taskService->deleteTask($id);
+
+        if (!$task) {
+            return response()->json([
+                "message" => "Task Not Found."
+            ], 404);
+        }
+
+        return response()->json([
+            "message" => "Task Deleted.",
+            "Task" => $task
+        ]);
     }
 }
